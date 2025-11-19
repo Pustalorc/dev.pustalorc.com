8ff78360f9944395c8c222869944d3a5e93bae3b
Pustalorc <github@pustalorc.com>
2025-11-19 13:26:37 -0500

    Update professional experience (I'm migrating to Astro and updating site design soon)

--- src/constants/skills/professionalExperience.js
+++ src/constants/skills/professionalExperience.js
@@ -1,21 +1,28 @@
 // ---------- PRIVATE ----------
 const professionalExperience = [
   {
-    company: 'Grant Thornton',
-    title: 'Senior Software Developer',
-    startYear: '2023'
+    company: "Pine Hosting",
+    title: "Senior Software Developer",
+    startYear: "2024"
   },
   {
-    company: 'Self-Employed',
-    title: 'Software Engineer',
-    startYear: '2016'
+    company: "Grant Thornton",
+    title: "Senior Software Developer",
+    startYear: "2023",
+    endYear: "2024",
   },
   {
-    company: 'Grant Thornton',
-    title: 'Software Developer',
-    startYear: '2022',
-    endYear: '2023'
-  }
+    company: "Self-Employed",
+    title: "Software Engineer",
+    startYear: "2016",
+    endYear: "2022",
+  },
+  {
+    company: "Grant Thornton",
+    title: "Software Developer",
+    startYear: "2022",
+    endYear: "2023",
+  },
 ];
 
 // ---------- PUBLIC ----------
